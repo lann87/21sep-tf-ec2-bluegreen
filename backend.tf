76d3d563dbfbb3f261040974527477466058d445
Alan Peh <alanpeh87@gmail.com>
2024-09-22 12:08:45 +0800

    update version

--- backend.tf
+++ backend.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.5.0"
   backend "s3" {
     bucket = "sctp-ce7-tfstate"        # Terraform State bucket name
     key    = "alanp-bluegreen-tfstate" # Name of your tfstate file
