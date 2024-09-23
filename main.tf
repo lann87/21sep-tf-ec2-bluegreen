3164ba2b8e7bc0da92ff8a9de2157e60a23b64f9
Alan Peh <alanpeh87@gmail.com>
2024-09-23 16:36:39 +0800

    updating format on main

--- main.tf
+++ main.tf
@@ -88,12 +88,12 @@ resource "aws_lb_listener" "app" {
     target_group_arn = aws_lb_target_group.blue.arn
     forward {
       target_group {
-        arn =     aws_lb_target_group.blue.arn
+        arn    = aws_lb_target_group.blue.arn
         weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
       }
 
       target_group {
-        arn =    aws_lb_target_group.green.arn
+        arn    = aws_lb_target_group.green.arn
         weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
       }
 
